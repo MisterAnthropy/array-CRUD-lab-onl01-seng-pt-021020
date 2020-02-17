e25b2c0396288488f827bcd962a885a6d8943e2b
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-17 02:34:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,8 @@ end
 
 def add_element_to_start_of_array(array, element)
     array = ["Juicy" , "Dry"]
-  element = "arrays!"
-  array.push(element)
+  element = "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
